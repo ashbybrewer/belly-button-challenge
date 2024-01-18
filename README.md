4786c4cf704957787e076b3cac740cd441cdfdca
ashbybrewer <143283503+ashbybrewer@users.noreply.github.com>
2024-01-18 11:24:29 -0600

    Final attempt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # belly-button-challenge
+
+I know this is being turned in late, and I'm pretty certain it's not done correctly. That said I'm just trying to get it turned in without any further delay. 
+I used ChatGPT 3.5, as well as some fellow students to help me on this assignmnet. I also looked at similar githubs working with this data as a reference point to work from. 
--- static/app.js
+++ static/app.js
@@ -1,4 +1,12 @@
-<script src="https://d3js.org/d3.v5.min.js"></script>
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Get the data directly from the URL and log to the console to view
+
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+  console.log("Belly Button Diversity Data: ", data);
+});
 
 function getPlots(id) {
     //Read samples.json
